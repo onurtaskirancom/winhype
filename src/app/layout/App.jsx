9e2ca63ff79ccb8135d2f0de216bcefdd68be1d4
onurtaskirancom <taskiranonur9@gmail.com>
2021-08-19 07:04:18 +0300

    Added Semantic UI  and react version down

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <h1>Win Hype</h1>
+      <button className='ui icon red button'>
+        <i className='user icon'/> CSS Button
+      </button>
+      <Button icon='user' content='React Button' color='green' />
     </div>
   );
 }
-
-export default App;
