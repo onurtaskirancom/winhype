2b7004d2dd645756790be7211f2758fca3ff0202
onurtaskirancom <taskiranonur9@gmail.com>
2021-08-20 08:11:20 +0300

    Added navigation bar and styling

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar';
 
 export default function App() {
   return (
-    <div>
-      <h1>Win Hype</h1>
-      <EventDashboard />
-    </div>
+    <>
+      <NavBar />
+      <Container className='main'>
+        <EventDashboard />
+      </Container>
+      
+    </>
   );
 }
