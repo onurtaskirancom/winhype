2be0a6f56f98c15feef1731bc02c23916e32cc92
onurtaskirancom <taskiranonur9@gmail.com>
2021-09-02 15:55:08 +0300

    Added fake authentication

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Button, Container, Menu } from 'semantic-ui-react';
-import SignedInMenu from './SignedInMenu';
-import SignedOutMenu from './SignedOutMenu';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Button, Container, Menu } from "semantic-ui-react";
+import SignedInMenu from "./SignedInMenu";
+import SignedOutMenu from "./SignedOutMenu";
 
-export default function NavBar({setFormOpen}) {
-    return (
-        <Menu inverted fixed='top'>
-            <Container>
-                <Menu.Item as={NavLink} exact to='/' header>
-                    <img src="/assets/logo.png" alt="logo" style={{marginRight: 15}}/>
-                    Win Hype
-                </Menu.Item>
-                <Menu.Item as={NavLink} to='/events' name='Events' />
-                <Menu.Item as={NavLink} to='/createEvent'>
-                    <Button positive inverted content='Create Event'/>
-                </Menu.Item>
-                <SignedOutMenu />
-                <SignedInMenu />
-            </Container>
-        </Menu>
-    )
+export default function NavBar({ setFormOpen }) {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  return (
+    <Menu inverted fixed='top'>
+      <Container>
+        <Menu.Item as={NavLink} exact to='/' header>
+          <img src='/assets/logo.png' alt='logo' style={{ marginRight: 15 }} />
+          Win Hype
+        </Menu.Item>
+        <Menu.Item as={NavLink} to='/events' name='Events' />
+        {authenticated && (
+          <Menu.Item as={NavLink} to='/createEvent'>
+            <Button positive inverted content='Create Event' />
+          </Menu.Item>
+        )}
+        {authenticated ? (
+          <SignedInMenu setAuthenticated={setAuthenticated} />
+        ) : (
+          <SignedOutMenu setAuthenticated={setAuthenticated} />
+        )}
+      </Container>
+    </Menu>
+  );
 }
