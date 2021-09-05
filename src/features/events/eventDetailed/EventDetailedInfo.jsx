0930c6f1aae38a6be1c2cd96c93bb956dcf0888c
onurtaskirancom <taskiranonur9@gmail.com>
2021-09-05 13:51:40 +0300

    Event Detailed Page structure

---
+++ src/features/events/eventDetailed/EventDetailedInfo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function EventDetailedInfo() {
+    return (
+        <h1>Info</h1>
+    )
+}
---
+++ src/features/events/eventDetailed/EventDetailedSidebar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function EventDetailedSidebar() {
+    return (
+        <h1>Sidebar</h1>
+    )
+}
