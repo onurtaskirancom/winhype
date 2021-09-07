2cca9d5fe94cebde2ec804f4ae34ebe7c602c0cf
onurtaskirancom <taskiranonur9@gmail.com>
2021-09-07 22:47:53 +0300

    Cleaning up the unused code

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
@@ -8,30 +8,18 @@ import HomePage from "../../features/home/HomePage";
 import NavBar from "../../features/nav/NavBar";
 
 export default function App() {
-  const [formOpen, setFormOpen] = useState(false);
-  const [selectedEvent, setSelectedEvent] = useState(null);
-
-  function handleSelectEvent(event) {
-    setSelectedEvent(event);
-    setFormOpen(true);
-  }
-
-  function handleCreateFormOpen() {
-    setSelectedEvent(null);
-    setFormOpen(true);
-  }
 
   return (
     <>
      <Route exact path='/' component={HomePage}/>
      <Route path={'/(.+)'} render={() => (
        <>
-            <NavBar setFormOpen={handleCreateFormOpen} />
+            <NavBar />
             <Container className='main'>
              
               <Route exact path='/events' component={EventDashboard}/>
               <Route path='/events/:id' component={EventDetailedPage}/>
-              <Route path='/createEvent' component={EventForm}/>
+              <Route path={['/createEvent', '/manage/:id']} component={EventForm}/>
             </Container>
        </>
 
