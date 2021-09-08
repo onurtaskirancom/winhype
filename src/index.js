006b95ee1be97872a691e2fa0bdd3cc575326d64
onurtaskirancom <taskiranonur9@gmail.com>
2021-09-08 20:33:42 +0300

    Setting up Redux

--- src/index.js
+++ src/index.js
@@ -5,15 +5,23 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from './app/store/configureStore';
+
+const store = configureStore()
 
 const rootEl = document.getElementById('root');
 
 function render() {
   ReactDOM.render(
+    <Provider store={store}>
     <BrowserRouter>
       <App />
-    </BrowserRouter>,
-    rootEl)
+    </BrowserRouter>
+    </Provider>
+  ,
+    rootEl
+    );
 }
 
 if (module.hot) {
