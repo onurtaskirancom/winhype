d17cbe95971c7f197b75eb02a7cba216eda5b83c
onurtaskirancom <taskiranonur9@gmail.com>
2021-10-29 15:40:48 +0300

    Isolating the loading indicators

--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -8,6 +8,7 @@ import { decrement, increment } from "./testReducer";
 
 export default function Sandbox() {
   const dispatch = useDispatch();
+  const [target, setTarget] = useState(null);
   const data = useSelector((state) => state.test.data);
   const {loading} = useSelector(state => state.async);
   const defaultProps = {
@@ -28,14 +29,22 @@ export default function Sandbox() {
       <h1>Testing 123</h1>
       <h3>The data is: {data} </h3>
       <Button
-        loading={loading}
-        onClick={() => dispatch(increment(20))}
+        name='increment'
+        loading={loading && target === 'increment'}
+        onClick={(e) => {
+          dispatch(increment(20));
+          setTarget(e.target.name)
+        }}
         content='Increment'
         color='green'
       />
       <Button
-        loading={loading}
-        onClick={() => dispatch(decrement(10))}
+        name='decrement'
+        loading={loading && target === 'decrement'}
+        onClick={(e) => {
+          dispatch(decrement(10));
+          setTarget(e.target.name)
+        }}
         content='Decrement'
         color='red'
       />
