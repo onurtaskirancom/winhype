3d7b34f3f755c9c91c9f278d90852e4396abe44b
onurtaskirancom <taskiranonur9@gmail.com>
2021-10-30 13:22:14 +0300

    Added toast notifications

--- src/features/sandbox/testReducer.js
+++ src/features/sandbox/testReducer.js
@@ -1,4 +1,6 @@
 import { asyncActionError, asyncActionFinish, asyncActionStart } from "../../app/async/asyncReducer";
+import { delay } from '../../app/common/util/util';
+import {toast} from 'react-toastify';
 
 const INCREMENT_COUNTER = "INCREMENT_COUNTER";
 const DECREMENT_COUNTER = "DECREMENT_COUNTER";
@@ -24,7 +26,8 @@ export function decrement(amount) {
       dispatch({type: DECREMENT_COUNTER, payload: amount});
       dispatch(asyncActionFinish());
     } catch (error) {
-      dispatch(asyncActionError(error))
+      dispatch(asyncActionError(error));
+      toast.error(error);
     }
   }
   
