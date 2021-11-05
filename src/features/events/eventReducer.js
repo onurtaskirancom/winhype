9ef2c2d4e7f33efde96de048e2e037d95d6c3d95
onurtaskirancom <taskiranonur9@gmail.com>
2021-11-05 20:06:16 +0300

    Added a mock API

--- src/features/events/eventReducer.js
+++ src/features/events/eventReducer.js
@@ -1,29 +1,36 @@
-import {sampleData} from '../../app/api/sampleData';
-import { CREATE_EVENT, DELETE_EVENT, UPDATE_EVENT } from './eventConstants';
+import { sampleData } from "../../app/api/sampleData";
+import { CREATE_EVENT, DELETE_EVENT, FETCH_EVENTS, UPDATE_EVENT } from "./eventConstants";
 
 const initialState = {
-    events: sampleData
-}
+  events: [],
+};
 
-export default function eventReducer(state = initialState, {type, payload}) {
-    switch (type) {
-        case CREATE_EVENT:
-            return {
-                ...state,
-                events: [...state.events, payload]
-            };
-            case UPDATE_EVENT:
-                return {
-                    ...state,
-                    events: [...state.events.filter(evt => evt.id !== payload.id), payload]
-                }
-                case DELETE_EVENT:
-                    return {
-                        ...state,
-                        events: [...state.events.filter(evt => evt.id !== payload)]
-                    }
-                    default:
-                        return state;
-
-    }
+export default function eventReducer(state = initialState, { type, payload }) {
+  switch (type) {
+    case CREATE_EVENT:
+      return {
+        ...state,
+        events: [...state.events, payload],
+      };
+    case UPDATE_EVENT:
+      return {
+        ...state,
+        events: [
+          ...state.events.filter((evt) => evt.id !== payload.id),
+          payload,
+        ],
+      };
+    case DELETE_EVENT:
+      return {
+        ...state,
+        events: [...state.events.filter((evt) => evt.id !== payload)],
+      };
+      case FETCH_EVENTS:
+          return {
+              ...state,
+              events: payload
+          }
+    default:
+      return state;
+  }
 }
