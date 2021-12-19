ffba5ccd53616797ffc9b3e2cf784d2524a4ad34
onurtaskirancom <taskiranonur9@gmail.com>
2021-12-19 18:57:26 +0300

    Added a useFirestoreDoc hook

--- src/features/events/eventDetailed/EventDetailedPage.jsx
+++ src/features/events/eventDetailed/EventDetailedPage.jsx
@@ -1,23 +1,40 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Grid } from 'semantic-ui-react';
-import EventDetailedChat from './EventDetailedChat';
-import EventDetailedHeader from './EventDetailedHeader';
-import EventDetailedInfo from './EventDetailedInfo';
-import EventDetailedSidebar from './EventDetailedSidebar';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Grid } from "semantic-ui-react";
+import { listenToEventFromFirestore } from "../../../app/firestore/firestoreService";
+import useFirestoreDoc from "../../../app/hooks/useFirestoreDoc";
+import EventDetailedChat from "./EventDetailedChat";
+import EventDetailedHeader from "./EventDetailedHeader";
+import EventDetailedInfo from "./EventDetailedInfo";
+import EventDetailedSidebar from "./EventDetailedSidebar";
+import { listenToEvents } from "../eventActions";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
-export default function EventDetailedPage ({match}) {
-    const event = useSelector(state => state.event.events.find(e => e.id === match.params.id));
-    return (
-        <Grid>
-            <Grid.Column width={10}>
-                <EventDetailedHeader event={event}/>
-                <EventDetailedInfo event={event}/>
-                <EventDetailedChat />
-            </Grid.Column>
-            <Grid.Column width={6}>
-                <EventDetailedSidebar attendees={event.attendees}/>
-            </Grid.Column>
-        </Grid>
-    )
+export default function EventDetailedPage({ match }) {
+  const dispatch = useDispatch();
+  const event = useSelector((state) =>
+    state.event.events.find((e) => e.id === match.params.id)
+  );
+  const { loading } = useSelector((state) => state.async);
+
+  useFirestoreDoc({
+    query: () => listenToEventFromFirestore(match.params.id),
+    data: (event) => dispatch(listenToEvents([event])),
+    deps: [match.params.id, dispatch],
+  });
+
+  if (loading || !event) return <LoadingComponent content='Loading Event...' />;
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <EventDetailedHeader event={event} />
+        <EventDetailedInfo event={event} />
+        <EventDetailedChat />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <EventDetailedSidebar attendees={event?.attendees} />
+      </Grid.Column>
+    </Grid>
+  );
 }
