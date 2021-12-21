b0873686feddc3b8e923ceaa80a5b2afa8433666
onurtaskirancom <taskiranonur9@gmail.com>
2021-12-21 23:47:52 +0300

    Handling not found documents

--- src/app/hooks/useFirestoreDoc.js
+++ src/app/hooks/useFirestoreDoc.js
@@ -10,6 +10,10 @@ export default function useFirestoreDoc({query, data, deps}) {
         dispatch(asyncActionStart());
         const unsubscribe = query().onSnapshot( 
             snapshot => {
+                if (!snapshot.exists) {
+                    dispatch(asyncActionError({code: 'not-found', message: 'Could not find document'}));
+                    return;
+                }
                 data(dataFromSnapshot(snapshot));
                 dispatch(asyncActionFinish());
             },
