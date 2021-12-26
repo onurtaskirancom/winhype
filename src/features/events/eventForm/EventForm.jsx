a9a64efcc8717055f4ff65598248fbddec0418e0
onurtaskirancom <taskiranonur9@gmail.com>
2021-12-27 00:18:23 +0300

    Creating and updating events in Firestore

--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -2,10 +2,10 @@
 import React from "react";
 import { Button, Header, Segment } from "semantic-ui-react";
 import cuid from "cuid";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { createEvent, updateEvent } from "../eventActions";
+import { createEvent, listenToEvents, updateEvent } from "../eventActions";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import MyTextInput from "../../../app/common/form/MyTextInput";
@@ -14,24 +14,29 @@ import MySelectInput from "../../../app/common/form/MySelectInput";
 import { categoryData } from "../../../app/api/categoryOptions";
 import MyDateInput from "../../../app/common/form/MyDateInput";
 import MyPlaceInput from "../../../app/common/form/MyPlaceInput";
+import { addEventToFirestore, listenToEventFromFirestore, updateEventInFirestore } from "../../../app/firestore/firestoreService";
+import useFirestoreDoc from "../../../app/hooks/useFirestoreDoc";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { toast } from "react-toastify";
 
 export default function EventForm({ match, history }) {
   const dispatch = useDispatch();
   const selectedEvent = useSelector((state) =>
     state.event.events.find((e) => e.id === match.params.id)
   );
+  const { loading, error } = useSelector((state) => state.async);
 
   const initialValues = selectedEvent ?? {
     title: "",
     category: "",
     description: "",
     city: {
-      address: '',
-      latLng: null
+      address: "",
+      latLng: null,
     },
     venue: {
-      address: '',
-      latLng: null
+      address: "",
+      latLng: null,
     },
     date: "",
   };
@@ -41,32 +46,42 @@ export default function EventForm({ match, history }) {
     category: Yup.string().required("You must provide a category"),
     description: Yup.string().required(),
     city: Yup.object().shape({
-      address: Yup.string().required('City is required')
+      address: Yup.string().required("City is required"),
     }),
     venue: Yup.object().shape({
-      address: Yup.string().required('Venue is required')
+      address: Yup.string().required("Venue is required"),
     }),
     date: Yup.string().required(),
   });
+
+  useFirestoreDoc({
+    query: () => listenToEventFromFirestore(match.params.id),
+    data: (event) => dispatch(listenToEvents([event])),
+    deps: [match.params.id, dispatch],
+  });
+
+  if (loading || (!selectedEvent && !error))
+    return <LoadingComponent content='Loading Event...' />;
+
+  if (error) return <Redirect to='/error' />;
 
   return (
     <Segment clearing>
       <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
-        onSubmit={(values) => {
-          selectedEvent
-            ? dispatch(updateEvent({ ...selectedEvent, ...values }))
-            : dispatch(
-                createEvent({
-                  ...values,
-                  id: cuid(),
-                  hostedBy: "Bob",
-                  attendees: [],
-                  hostPhotoURL: "/assets/user.png",
-                })
-              );
+        onSubmit={async (values, {setSubmitting}) => {
+          try {
+            selectedEvent
+            ? await updateEventInFirestore(values)
+            : await addEventToFirestore(values);
+            setSubmitting(false);
           history.push("/events");
+          } catch (error) {
+            toast.error(error.message);
+            setSubmitting(false);
+          }
+         
         }}
       >
         {({ isSubmitting, dirty, isValid, values }) => (
@@ -81,15 +96,14 @@ export default function EventForm({ match, history }) {
             <MyTextArea name='description' placeholder='Description' rows={3} />
             <Header sub color='teal' content='Event Location Details' />
             <MyPlaceInput name='city' placeholder='City' />
-            <MyPlaceInput 
-              name='venue' 
+            <MyPlaceInput
+              name='venue'
               disabled={!values.city.latLng}
-              placeholder='Venue' 
-              options = {{
+              placeholder='Venue'
+              options={{
                 location: new google.com.maps.latLng(values.city.latLng),
                 radius: 1000,
-                types: ['establishment']
-
+                types: ["establishment"],
               }}
             />
             <MyDateInput
